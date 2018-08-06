8b97cc344af5e69e62bf3c402520aa57ae47036a
Aliaksei Rekish <alexeyrekish@gmail.com>
2018-08-06 19:26:59 +0300

    users and auth routes done

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "airbnb-base",
+    rules: {
+      'no-underscore-dangle': 'off',
+      'no-plusplus': 'off',
+      'func-names': 'off',
+      'consistent-return': 'off',
+      'no-console': 'off',
+      'global-require': 'off'
+    },
+    "env": {
+      "jest": true
+    }
+};
