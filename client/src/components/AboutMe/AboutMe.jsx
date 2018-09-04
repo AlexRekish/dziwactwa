a31c424eda00c5db440cfb9b697c6f74a6575e20
AlexRekish <alexeyrekish@gmail.com>
2018-09-04 16:34:50 +0300

    fixed scrolls

--- client/src/components/AboutMe/AboutMe.jsx
+++ client/src/components/AboutMe/AboutMe.jsx
@@ -1,32 +1,29 @@
 import React from 'react';
-import { Scrollbars } from 'react-custom-scrollbars';
 import './AboutMe.sass';
 // import placeholder from '../../assets/photo.jpg';
 
 const AboutMe = () => (
   <section className="about-me">
     <div className="about-me__content-wrapper">
-      <Scrollbars autoHide autoHideTimeout={1000} autoHideDuration={200}>
-        <h1 className="about-me__greetings">About Me</h1>
-        <p className="about-me__content">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam molestias assumenda laborum
-          labore inventore consectetur minus quo, corporis illo dolorem tempore voluptatum qui
-          aperiam accusamus facere mollitia a ducimus eligendi excepturi reiciendis exercitationem
-          esse! Est ipsum tenetur recusandae nihil eius facilis blanditiis reiciendis. Quos,
-          doloremque provident minus earum voluptatum iste corrupti, quae perferendis eum, maiores
-          animi nihil hic vero ducimus pariatur asperiores a ad iure magnam quia modi? Odit fugit ut
-          dignissimos cupiditate quae vero possimus porro! Eius beatae non illum quae, laborum
-          laudantium repudiandae fuga deleniti cupiditate mollitia consectetur dolores porro sint
-          inventore incidunt ut, dolorem quibusdam, quis placeat nostrum quisquam. In corporis
-          consequuntur aliquam harum tenetur maxime quae hic odio neque quasi dolorem culpa, quidem
-          voluptatum, explicabo doloribus eos placeat illum. Incidunt laudantium molestias officia
-          animi esse quasi accusantium officiis sapiente aspernatur nihil nesciunt similique aliquam
-          deserunt ea dolor rem ratione, assumenda, error, possimus rerum adipisci! Illo, dolores?
-        </p>
-        {/* <div className="photo-wrapper">
+      <h1 className="about-me__greetings">About Me</h1>
+      <p className="about-me__content">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam molestias assumenda laborum
+        labore inventore consectetur minus quo, corporis illo dolorem tempore voluptatum qui aperiam
+        accusamus facere mollitia a ducimus eligendi excepturi reiciendis exercitationem esse! Est
+        ipsum tenetur recusandae nihil eius facilis blanditiis reiciendis. Quos, doloremque
+        provident minus earum voluptatum iste corrupti, quae perferendis eum, maiores animi nihil
+        hic vero ducimus pariatur asperiores a ad iure magnam quia modi? Odit fugit ut dignissimos
+        cupiditate quae vero possimus porro! Eius beatae non illum quae, laborum laudantium
+        repudiandae fuga deleniti cupiditate mollitia consectetur dolores porro sint inventore
+        incidunt ut, dolorem quibusdam, quis placeat nostrum quisquam. In corporis consequuntur
+        aliquam harum tenetur maxime quae hic odio neque quasi dolorem culpa, quidem voluptatum,
+        explicabo doloribus eos placeat illum. Incidunt laudantium molestias officia animi esse
+        quasi accusantium officiis sapiente aspernatur nihil nesciunt similique aliquam deserunt ea
+        dolor rem ratione, assumenda, error, possimus rerum adipisci! Illo, dolores?
+      </p>
+      {/* <div className="photo-wrapper">
         <img src={placeholder} alt="placeholder" className="photo" />
       </div> */}
-      </Scrollbars>
     </div>
   </section>
 );
