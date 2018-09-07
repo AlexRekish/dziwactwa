4e40dcb0df7034d78c59234328e8f984b17fb564
AlexRekish <alexeyrekish@gmail.com>
2018-09-07 11:52:09 +0300

    added touch events for the lightbox

--- client/src/containers/Gallery/LightBox/LightBox.jsx
+++ client/src/containers/Gallery/LightBox/LightBox.jsx
@@ -1,61 +1,113 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './LightBox.sass';
 
-const LightBox = ({
-  src,
-  title,
-  date,
-  user,
-  current,
-  count,
-  isOpen,
-  onNext,
-  onPrev,
-  onClose,
-  onDelete
-}) => (
-  <div className="lightbox" style={isOpen ? { display: 'grid' } : { display: 'none' }}>
-    <button type="button" className="lightbox__control lightbox__control--left" onClick={onPrev}>
-      <span className="visually-hidden">prev</span>
-      <FontAwesomeIcon icon="chevron-left" />
-    </button>
-    <article className="lightbox__item">
-      <div className="lightbox__photo-wrapper">
-        <img src={src} alt={title} className="lightbox__photo" />
+class LightBox extends Component {
+  state = {
+    xStart: null
+  };
+
+  touchStartHandler = evt => {
+    const { xStart } = this.state;
+    if (evt.changedTouches.length !== 1 || xStart !== null) return;
+    this.setState({ xStart: evt.changedTouches[0].clientX });
+  };
+
+  touchEndHandler = evt => {
+    const { xStart } = this.state;
+    const { onNext, onPrev } = this.props;
+
+    if (xStart === null) return;
+
+    const xEnd = evt.changedTouches[0].clientX;
+    const xDiff = xStart - xEnd;
+    if (Math.abs(xDiff) > 100) {
+      if (xDiff < 0) {
+        onNext();
+        this.setState({
+          xStart: null
+        });
+      }
+      if (xDiff > 0) {
+        onPrev();
+        this.setState({
+          xStart: null
+        });
+      }
+    }
+  };
+
+  render() {
+    const {
+      src,
+      title,
+      date,
+      user,
+      current,
+      count,
+      isOpen,
+      onNext,
+      onPrev,
+      onClose,
+      onDelete
+    } = this.props;
+    return (
+      <div
+        className="lightbox"
+        style={isOpen ? { display: 'grid' } : { display: 'none' }}
+        onTouchStart={this.touchStartHandler}
+        onTouchEnd={this.touchEndHandler}
+      >
         <button
           type="button"
-          className="lightbox__button lightbox__button--close"
-          onClick={onClose}
+          className="lightbox__control lightbox__control--left"
+          onClick={onPrev}
         >
-          <span className="visually-hidden">close</span>
-          <FontAwesomeIcon icon="times" />
+          <span className="visually-hidden">prev</span>
+          <FontAwesomeIcon icon="chevron-left" />
         </button>
-        {user &&
-          user.isAdmin && (
+        <article className="lightbox__item">
+          <div className="lightbox__photo-wrapper">
+            <img src={src} alt={title} className="lightbox__photo" />
             <button
               type="button"
-              className="lightbox__button lightbox__button--delete"
-              onClick={onDelete}
+              className="lightbox__button lightbox__button--close"
+              onClick={onClose}
             >
               <span className="visually-hidden">close</span>
-              <FontAwesomeIcon icon="trash-alt" />
+              <FontAwesomeIcon icon="times" />
             </button>
-          )}
+            {user &&
+              user.isAdmin && (
+                <button
+                  type="button"
+                  className="lightbox__button lightbox__button--delete"
+                  onClick={onDelete}
+                >
+                  <span className="visually-hidden">close</span>
+                  <FontAwesomeIcon icon="trash-alt" />
+                </button>
+              )}
+          </div>
+          <div className="lightbox__photo-info">
+            <h2 className="lightbox__photo-title">{title}</h2>
+            <p className="lightBox__photo-date">{date}</p>
+            <p className="lightbox__counter">{`${current}/${count}`}</p>
+          </div>
+        </article>
+        <button
+          type="button"
+          className="lightbox__control lightbox__control--right"
+          onClick={onNext}
+        >
+          <span className="visually-hidden">next</span>
+          <FontAwesomeIcon icon="chevron-right" />
+        </button>
       </div>
-      <div className="lightbox__photo-info">
-        <h2 className="lightbox__photo-title">{title}</h2>
-        <p className="lightBox__photo-date">{date}</p>
-        <p className="lightbox__counter">{`${current}/${count}`}</p>
-      </div>
-    </article>
-    <button type="button" className="lightbox__control lightbox__control--right" onClick={onNext}>
-      <span className="visually-hidden">next</span>
-      <FontAwesomeIcon icon="chevron-right" />
-    </button>
-  </div>
-);
+    );
+  }
+}
 
 LightBox.propTypes = {
   src: PropTypes.string,
